e1ecd73b9af943d2dd26ab51882446095d945fd6
minju0426 <kimminju04260@gmail.com>
2022-11-28 16:21:31 +0900

    Docs: setUpJetpackNavigation()

--- app/src/main/java/com/example/booksearch/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/booksearch/ui/view/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.booksearch.R
 import com.example.booksearch.data.repository.BookSearchRepositoryImpl
@@ -20,22 +21,15 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
     lateinit var bookViewModel: BookSearchViewModel
+    private lateinit var appBarConfig: AppBarConfiguration
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-        binding.bottomNavView.apply {
-            setupWithNavController(navController) //id 자동 매핑해줌
-            isItemHorizontalTranslationEnabled
-        }
-//
-//        binding.bottomNavView.setupWithNavController(navController)
-//        binding.bottomNavView.isItemHorizontalTranslationEnabled
+        setUpJetpackNavigation()
+
         //viewModel
         val bookRepostoryImpl = BookSearchRepositoryImpl()
         //Impl & owner 지정
@@ -46,10 +40,34 @@ class MainActivity : AppCompatActivity() {
             factory
         )[BookSearchViewModel::class.java] //run-time에 프로그램에서 클래스 알도록
 
-
     }
 
+    private fun setUpJetpackNavigation() {
+        //nav_host_fragment 없을 시 리턴
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+                ?: return
 
+        navController = navHostFragment.navController
+
+        binding.bottomNavView.apply {
+            setupWithNavController(navController) //id 자동 매핑해줌 -> 프래그먼트전환해줌
+            isItemHorizontalTranslationEnabled
+        }
+        /*
+        appBarConfig = AppBarConfiguration(
+            //모든 Fragment SET top level로 설정
+            setOf(
+                R.id.searchFragment, R.id.favoriteFragment, R.id.settingsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfig)*/
+    }
+/*
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
+*/
 /*
     //bottom navigation 리스너 구현 (xml에 framelayout 생성 시)
     private fun setUpBottomNavigationView() {

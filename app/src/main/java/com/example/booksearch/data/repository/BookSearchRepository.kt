add678cd6860b27d3985f0b69bc317dd7418cdca
minju0426 <kimminju04260@gmail.com>
2022-12-01 00:15:32 +0900

    override DAO fun

--- app/src/main/java/com/example/booksearch/data/repository/BookSearchRepository.kt
+++ app/src/main/java/com/example/booksearch/data/repository/BookSearchRepository.kt
@@ -1,13 +1,21 @@
 package com.example.booksearch.data.repository
 
+import androidx.lifecycle.LiveData
+import com.example.booksearch.data.model.Book
 import com.example.booksearch.data.model.BookSearchResponse
 import retrofit2.Response
 
 interface BookSearchRepository {
+
     suspend fun searchBooks( //일시중단가능한 함수
         query: String,
         sort: String,
         page: Int,
         size: Int
     ): Response<BookSearchResponse> //응답은 BookSearchResponse 형태로
+
+    // Room
+    suspend fun insertBooks(book: Book)
+    suspend fun deleteBooks(book: Book)
+    fun getFavoriteBooks(): LiveData<List<Book>>
 }
--- app/src/main/java/com/example/booksearch/data/repository/BookSearchRepositoryImpl.kt
+++ app/src/main/java/com/example/booksearch/data/repository/BookSearchRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.example.booksearch.data.repository
 
+import androidx.lifecycle.LiveData
 import com.example.booksearch.api.RetrofitInstance
+import com.example.booksearch.data.db.BookDatabase
+import com.example.booksearch.data.model.Book
 import com.example.booksearch.data.model.BookSearchResponse
 import retrofit2.Response
 
 //BookSearchRepository 인터페이스를 구현할 클래스
-class BookSearchRepositoryImpl : BookSearchRepository {
+class BookSearchRepositoryImpl(private val db: BookDatabase) : BookSearchRepository {
 
     override suspend fun searchBooks(
         query: String,
@@ -14,6 +17,18 @@ class BookSearchRepositoryImpl : BookSearchRepository {
         size: Int
     ): Response<BookSearchResponse> {
         return RetrofitInstance.api.searchBooks(query, sort, page, size) //api실행하여 반환값 4개 받기
+    }
+
+    override suspend fun insertBooks(book: Book) {
+        db.bookDao().insertBook(book)
+    }
+
+    override suspend fun deleteBooks(book: Book) {
+        db.bookDao().deleteBook(book)
+    }
+
+    override fun getFavoriteBooks(): LiveData<List<Book>> {
+        return db.bookDao().getFavoriteBooks()
     }
 
 }

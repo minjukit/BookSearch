3267e58ef8d24139e4e275d74f1657e9bb52d3d2
minju0426 <kimminju04260@gmail.com>
2022-12-01 00:05:12 +0900

    Add serialization TypeConverter

--- app/src/main/java/com/example/booksearch/data/db/BookDatabase.kt
+++ app/src/main/java/com/example/booksearch/data/db/BookDatabase.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.booksearch.data.model.Book
 
 //https://github.com/android/architecture-components-samples/blob/main/BasicRxJavaSampleKotlin/app/src/main/java/com/example/android/observability/persistence/UsersDatabase.kt
 
 @Database(entities = [Book::class], version = 1, exportSchema = false) //room에서 사용할 엔티티,버전 설정
+@TypeConverters(OrmConverter::class) //컨버터 생성 후 룸데이터베이스에 등록해주기
 abstract class BookDatabase : RoomDatabase() {
     abstract fun bookDao(): BookSearchDAO
 
---
+++ app/src/main/java/com/example/booksearch/data/db/OrmConverter.kt
@@ -0,0 +1,23 @@
+package com.example.booksearch.data.db
+
+import androidx.room.TypeConverter
+import com.example.booksearch.data.model.Book
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class OrmConverter {
+
+    /* Room은 일반객체를 사용하지 못하기 때문에 authors의 type을 converter
+    * list를 받으면 String으로 인코드
+    * 스트링 받으면 List로 디코드
+    * 컨버터 생성 후 룸데이터베이스에 등록해주기
+     */
+
+    @TypeConverter
+    fun fromList(value: List<String>) = Json.encodeToString(value)
+
+    @TypeConverter
+    fun toList(value: String) = Json.decodeFromString<List<Book>>(value)
+
+}
